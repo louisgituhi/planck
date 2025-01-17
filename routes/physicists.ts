ef94c7bbeccfd4406a2d980b7f45d871d31c5340
louis <louisgituhi@gmail.com>
2025-01-17 19:36:28 +0300

    major redesign and error handling

--- routes/physicists.ts
+++ routes/physicists.ts
@@ -1,37 +1,35 @@
 import { Hono } from "hono";
 import { scientistsData } from "./data";
-import { z } from "zod";
 
 const physicist = new Hono();
 
-// get all physicists
+// home page of scientists
 physicist.get("/", (c) => {
+	return c.text("Welcome to the scientists API!");
+});
+
+// get all scientists
+physicist.get("/scientists", (c) => {
 	return c.json(scientistsData);
 });
 
-physicist.get("/random", (c) => {
+// get random scientits
+physicist.get("/scientists/random", (c) => {
 	return c.json(
 		scientistsData[Math.floor(Math.random() * scientistsData.length)],
 	);
 });
 
-// not found error
-
-physicist.notFound((c) => {
-	return c.text("Could not find the scientist you are looking for", 404);
-});
-
-// error handling
-physicist.onError((err, c) => {
-	return c.text(`Error occurred ${err.message}: ${err.name}`, 500);
-});
-
-physicist.get("/:id", (c) => {
-	const scientistId = c.req.param("id");
-	const response = scientistsData.find(
-		(scientist) => scientist.id.toString() === scientistId,
+// specific scientitist
+physicist.get("/scientists/:scientist", (c) => {
+	if (!c.req.param("scientist")) {
+		return c.text("No scientist name provided");
+	}
+	const scientist = scientistsData.find(
+		(person) =>
+			person.name.toLowerCase() === c.req.param("scientist").toLowerCase(),
 	);
-	return c.json(response);
+	return scientist ? c.json(scientist) : c.notFound();
 });
 
 export default physicist;
